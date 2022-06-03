6d8a508adcee081647a6fd40b139e5b2c8d8972a
603-kousei ninomiya <75236360+kousei-py-tech@users.noreply.github.com>
2022-06-03 16:08:21 +0900

    Revert "item table create"

--- app/app.py
+++ app/app.py
@@ -1,31 +1,11 @@
-import json
-import pymysql
 from flask import Flask
 
 app = Flask(__name__)
 
 
-def getConnection():
-    return pymysql.connect(
-        host='localhost',
-        user='app',
-        password='app',
-        database='app',
-        cursorclass=pymysql.cursors.DictCursor)
-
-@ app.route('/')
+@app.route('/')
 def hello():
     return "<h1>Hello Flask</h1>\n"
-
-@ app.route('/users')
-def users():
-    connection = getConnection()
-    with connection:
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM `user`"
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            return json.dumps(result)
 
 
 if __name__ == "__main__":
